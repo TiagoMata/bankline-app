685f50127e3a82e5f27847dbba735b0b6b7d3b11
TiagoMata <56051094+TiagoMata@users.noreply.github.com>
2022-05-25 22:21:21 -0300

    atualizando

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8080/swagger-ui/index.html';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovimentacaoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  list(): Observable<any> {
+    return this.http.get(`${baseUrl}/movimentacoes`);
+  }   
+create(movimentacao:any): Observable<any> {
+  return this.http.post(`${baseUrl}/movimentacoes`,movimentacao);
+
+}
 }
